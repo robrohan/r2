9fb28d53e48a373bd5f7ccbc6a06454af25b0462
Rob Rohan <rob@therohans.com>
2023-10-11 19:19:25 +1300

    Adding a utf8 string lib

--- tests/r2_strings.c
+++ tests/r2_strings.c
@@ -1,10 +1,8 @@
 #include "../r2_strings.h"
 #include "../r2_unit.h"
-#include <limits.h>
-#include <stdio.h>
+
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <stdio.h>
 
 #if (__WORDSIZE == 64)
 #define BUILD_64 1
@@ -14,16 +12,44 @@
 static char* test_create_string()
 {
     s8 str = S("This is a test");
-
     r2_assert("string length is wrong", (str.len == 14));
-
+    r2_assert("string size is wrong", (str.size == 14));
     return 0;
 }
 
+static char* test_create_string_blank()
+{
+    s8 str = S("");
+    r2_assert("string length is wrong", (str.len == 0));
+    r2_assert("string size is wrong", (str.size == 0));
+    return 0;
+}
+
+static char* test_create_string_null()
+{
+    char *str1 = (char *)malloc(sizeof(char *) * 10);
+    s8 str = S(str1);
+
+    r2_assert("null string length is wrong", (str.len == 0));
+    r2_assert("null string size is wrong", (str.size == 0));
+    return 0;
+}
+
+static char* test_create_utf8_string()
+{
+    s8 str = S("👋👏他是我的😺");
+    r2_assert("utf8 string length is wrong", (str.len == 7));
+    r2_assert("utf8 string size is wrong", (str.size == 24));
+    return 0;
+}
+
+////////////////////////////////////////////
 
 static char *r2_strings_test()
 {
     r2_run_test(test_create_string);
-
+    r2_run_test(test_create_utf8_string);
+    r2_run_test(test_create_string_blank);
+    r2_run_test(test_create_string_null);
     return 0;
 }
